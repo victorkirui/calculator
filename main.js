6fac0754eb2bc096d048b66e2f0f0ed73cedbe57
victorkirui <vkkiprop@gmail.com>
2024-08-06 11:32:38 +0300

    Create add,subtract,multiply,divide and operate functions

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert("hello world");
+// add
+let add = (num1, num2) => num1 + num2;
+// subtract
+let subtract = (num1, num2) => num1 - num2;
+// multiply
+let multiply = (num1, num2) => num1 * num2;
+// divide
+let divide = (num1, num2) => num1 / num2;
+
+let num1, operator, num2;
+
+let operate = (operator, num1, num2) => {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  }
+};
+
+alert(operate("*", 9, 3));
